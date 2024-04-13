0dae7e30c2bde7d70773b32baef6962b72fea588
rayhangafoor <35rayhan@gmail.com>
2024-04-13 11:32:47 +0530

    first commit

---
+++ src/test/java/com/korabank/oauth/OauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korabank.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
